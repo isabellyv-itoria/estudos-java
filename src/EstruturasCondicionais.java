278397eb0ee5376fcd5bdc0b2a0ef0f6609da8e3
isabellyv-itoria <isabellysouzanas@gmail.com>
2026-03-25 20:40:33 -0300

    Aprendizagem: if, else, else if e operador ternario

--- src/EstruturasCondicionais.java
+++ src/EstruturasCondicionais.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+/* import  java.util.Scanner;
 
 public class EstruturasCondicionais {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Sua idade: ");
+        int idade = input.nextInt();
+        String categoria;
+
+        if(idade < 15 ){
+            categoria = "Infantil";
+        } else if (idade >= 15 && idade < 18) {
+            categoria = "Juvenil";
+        } else{
+            categoria = "Adulto";
+        }
+
+        System.out.println(categoria);
+    }
+}*/
+
+import java.util.Scanner;
+
+public class EstruturasCondicionais {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String positivo = "positivo";
+        String negativo = "negativo";
+
+        System.out.println("Sua inteiro: ");
+        int inteiro = input.nextInt();
+
+        String resultado = inteiro > 0 ? positivo : negativo ;
+
+        System.out.println(resultado);
+    }
+
 }
